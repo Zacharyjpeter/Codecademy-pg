c2c22f1d9e76bb2ee5aad2e42f2f7c62c3c931a6
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-03 23:12:56 -0500

    learned relation btw object and method

--- playground.js
+++ playground.js
@@ -3,3 +3,8 @@ console.log('Codecademy'.toUpperCase());
 
 // Use a string method to log the following string without whitespace at the beginning and end of it.
 console.log('    Remove whitespace   '.trim());
+
+//Use built in objects and methods to do math calculations
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
