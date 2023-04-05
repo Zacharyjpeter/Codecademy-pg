cec3cb35e531f9bfd743833927c0a46c2654aa3a
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-04 22:39:36 -0500

    getting the hang of this!

--- playground.js
+++ playground.js
@@ -1,10 +1,21 @@
-// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+// Uses a built in method .toUpperCase() to print 'Codecademy' in all uppercase letters
 console.log('Codecademy'.toUpperCase());
 
-// Use a string method to log the following string without whitespace at the beginning and end of it.
+// Uses a built in string method to print the string without whitespace
 console.log('    Remove whitespace   '.trim());
+//outputs Remove whitespace 
 
-//Use built in objects and methods to do math calculations
+//Uses built in objects and methods to do math calculations
 console.log(Math.floor(Math.random() * 100));
 console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
+//outputs three things. first one is nested and produces a random whole number between 1-100
+//second one outputs the nearest whole number above 43.8
+//third one is a boolean check for integers; the answer is true
+
+//Uses var to create variables and assign them a value
+var favoriteFood = 'pizza';
+var numOfSlices = 8;
+console.log(favoriteFood);
+console.log(numOfSlices);
+//outputs pizza and 8 to the console
