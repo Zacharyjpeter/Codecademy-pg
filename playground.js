a9b4f3c970c47ae3107217073cb9f03a63bcfc68
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-06 20:28:34 -0500

    reviewed math operators

--- playground.js
+++ playground.js
@@ -31,3 +31,22 @@ const entree = 'Enchiladas';
 console.log(entree);
 entree = 'Tacos';
 //output = Enchiladas
+
+//Variable assignments listed here
+let levelUp = 10;
+let powerLevel = 9001;
+let multiplyMe = 32;
+let quarterMe = 1152;
+
+//Changing the variables here changes their output below
+levelUp += 5;
+powerLevel -= 100;
+multiplyMe *= 11;
+quarterMe /= 4;
+
+// These console.log() statements below will help you check the values of the variables.
+//Ouptuts here will update automatically based on changes above
+console.log("The value of levelUp:", levelUp);
+console.log("The value of powerLevel:", powerLevel);
+console.log("The value of multiplyMe:", multiplyMe);
+console.log("The value of quarterMe:", quarterMe);
