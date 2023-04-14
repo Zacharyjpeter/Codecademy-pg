076c89807b0f938e2b4bb31ef1098049cb9eabfd
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-14 15:56:46 -0500

    learned truthy falsy

--- playground.js
+++ playground.js
@@ -119,3 +119,45 @@ if (hungerLevel > 7) {
   console.log('We can eat later!');
 }
 //output We can eat later bc 7 is not greater than 7
+
+//logical operators allow an output to be based on condition
+let mood = 'sleepy';
+let tirednessLevel = 6;
+
+if (mood === 'sleepy' && tirednessLevel > 8) {
+  console.log('time to sleep');
+} else {
+  console.log('not bed time yet');
+}
+//output not bed time yet bc tiredness isn't high enough
+
+//tricky one. lets you know if variable is truthy or falsy
+//if the variable has a value it's truthy. if not it's falsy
+//variable cannot be 0, null, undefined, or NaN. else falsy
+let wordCount = 1;
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+
+let favoritePhrase = '';
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
+//output better get to work
+//output this string is def empty
+
+//another example of truthy vs falsy
+let tool = 'marker';
+
+// Use short circuit evaluation to assign  writingUtensil variable below:
+let writingUtensil = tool || 'pen';
+
+console.log(`The ${writingUtensil} is mightier than the sword.`);
+//output is marker bc tool has value. if tool was blank output would be pen
