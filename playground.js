de5f30551700e58e90d0ffdfc7f665a2895d4d91
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-05-02 11:28:08 -0500

    learned about global scope

--- playground.js
+++ playground.js
@@ -349,3 +349,14 @@ function logCitySkyline() {
 }
 
 console.log(logCitySkyline());
+
+//Global scope means the code block can be used anywhere in the file
+const satellite = "The Moon";
+const galaxy = "The Milky Way";
+const stars = "North Star";
+
+function callMyNightSky() {
+  return "Night Sky: " + satellite + ", " + stars + ", and " + galaxy;
+}
+
+console.log(callMyNightSky());
