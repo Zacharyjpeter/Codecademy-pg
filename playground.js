9292accba400ee31fe6b8d3e97b5a1b3ffa34192
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-05-30 13:35:39 -0500

    object literals can nest objects

--- playground.js
+++ playground.js
@@ -861,3 +861,49 @@ const alienShip = {
 
 alienShip.retreat();
 alienShip.takeOff();
+
+//object literals can have objects nested inside. this can get complex!
+let spaceship = {
+  passengers: null,
+  telescope: {
+    yearBuilt: 2018,
+    model: "91031-XLT",
+    focalLength: 2032,
+  },
+  crew: {
+    captain: {
+      name: "Sandra",
+      degree: "Computer Engineering",
+      encourageTeam() {
+        console.log("We got this!");
+      },
+      "favorite foods": ["cookies", "cakes", "candy", "spinach"],
+    },
+  },
+  engine: {
+    model: "Nimbus2000",
+  },
+  nanoelectronics: {
+    computer: {
+      terabytes: 100,
+      monitors: "HD",
+    },
+    "back-up": {
+      battery: "Lithium",
+      terabytes: 50,
+    },
+  },
+};
+
+let capFave = spaceship.crew.captain["favorite foods"][0];
+spaceship.passengers = [
+  {
+    name: "Caleb",
+    degree: "Rocket Science",
+    hypeMan() {
+      console.log("Time to blast off!");
+    },
+  },
+];
+
+let firstPassenger = spaceship.passengers[0];
