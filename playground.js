344d70922fefa9db88133dd3262a070b12a41591
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-06-05 19:25:33 -0500

    barely learned for in loop

--- playground.js
+++ playground.js
@@ -932,3 +932,46 @@ console.log(spaceship);
 //output: { 'Fuel Type': 'avocado oil',
 // homePlanet: 'Earth',
 // disabled: true }
+
+//for...in loops are used to iterate through an unordered array
+let spaceship = {
+  crew: {
+    captain: {
+      name: "Lily",
+      degree: "Computer Engineering",
+      cheerTeam() {
+        console.log("You got this!");
+      },
+    },
+    "chief officer": {
+      name: "Dan",
+      degree: "Aerospace Engineering",
+      agree() {
+        console.log("I agree, captain!");
+      },
+    },
+    medic: {
+      name: "Clementine",
+      degree: "Physics",
+      announce() {
+        console.log(`Jets on!`);
+      },
+    },
+    translator: {
+      name: "Shauna",
+      degree: "Conservation Science",
+      powerFuel() {
+        console.log("The tank is full!");
+      },
+    },
+  },
+};
+
+for (let crewMember in spaceship.crew) {
+  console.log(`${crewMember}: ${spaceship.crew[crewMember].name}`);
+}
+//output:
+//captain: Lily
+//chief officer: Dan
+//medic: Clementine
+//translator: Shauna
