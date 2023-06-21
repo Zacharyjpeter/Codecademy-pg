7b5f68f46da85c175dcf228b36acc9a005cc2c9b
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-06-20 23:18:02 -0500

    learned factory functions

--- playground.js
+++ playground.js
@@ -1043,3 +1043,42 @@ const arjun = {
 };
 
 console.log(arjun.energyLevel);
+
+//setters are similar to getters. I understand HOW but not WHY. The use case isn't clear
+const robot = {
+  _model: "1E78V2",
+  _energyLevel: 100,
+  _numOfSensors: 15,
+  get numOfSensors() {
+    if (typeof this._numOfSensors === "number") {
+      return this._numOfSensors;
+    } else {
+      return "Sensors are currently down.";
+    }
+  },
+  set numOfSensors(num) {
+    if (typeof num === "number" && num >= 0) {
+      this._numOfSensors = num;
+    } else {
+      console.log("Pass in a number that is greater than or equal to 0");
+    }
+  },
+};
+
+robot.numOfSensors = 100;
+console.log(robot.numOfSensors)
+
+//factory functions are used to create multiple objects simultaneously
+const robotFactory = (model, mobile) => {
+  return {
+    model: model,
+    mobile: mobile,
+    beep() {
+      console.log("Beep Boop");
+    },
+  };
+};
+
+const tinCan = robotFactory("P-500", true);
+tinCan.beep();
+//output beep boop
